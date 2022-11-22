412616dead459d9f72be8c1505c2152703646e83
diogoaav <116834699+diogoaav@users.noreply.github.com>
2022-11-22 14:37:43 -0500

    new app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = """Helloooo customer {name}!
+    html = """Helloooo cust0mer {name}!
     Hostname: {hostname}"""
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
